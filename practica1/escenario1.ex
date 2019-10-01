f4ea02d96c675cdbd9f652ce0ce4b62bae468fb4
diego <diego@localhost.localdomain>
2019-10-01 17:23:49 +0200

    Añadido estructura escenario1.ex

--- practica1/escenario1.ex
+++ practica1/escenario1.ex
@@ -2,4 +2,14 @@
 # NIAs: 739729, 755232
 # FICHERO: escenario1.ex
 # FECHA: 27-09-2019
-# DESCRIPCIÓN: 
+# DESCRIPCIÓN:
+
+defmodule ArquitecturaUno do
+  def iniciarServidor do
+    #Pone en marcha la máquina servidor
+  end
+
+  def iniciarCliente do
+    #Pone en marcha la máquina cliente
+  end
+end
